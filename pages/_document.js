6929839dc62fb48e9b19a691343a24fb47485242
Håkon Krogh <hakonhk@gmail.com>
2019-05-18 21:09:32 +0200

    Now v2

--- pages/_document.js
+++ pages/_document.js
@@ -1,31 +1,69 @@
-import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  render () {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const main = sheet.collectStyles(<Main />);
-    const styleTags = sheet.getStyleElement();
+    const originalRenderPage = ctx.renderPage;
 
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
     return (
       <html>
         <Head>
-          <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width, minimal-ui" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, user-scalable=no, width=device-width, minimal-ui"
+          />
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <link rel="apple-touch-icon" sizes="57x57" href="/static/favicons/apple-touch-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/static/favicons/apple-touch-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="/static/favicons/apple-touch-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="/static/favicons/apple-touch-icon-144x144.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/static/favicons/apple-touch-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/static/favicons/apple-touch-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/static/favicons/apple-touch-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/static/favicons/apple-touch-icon-144x144.png"
+          />
           <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
-          {styleTags}
         </Head>
         <body style={{ margin: 0 }}>
-          <div className='root'>
-            {main}
-          </div>
+          <Main />
+          <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,fetch" />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
